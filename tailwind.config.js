a26c2830b560c7c28f5d22e3d0dbb955cc0117e7
herradaj <javier.herrada@roche.com>
2025-04-25 13:54:20 +0200

    fixes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./nplh-message-handler-ui/index.html",
+    "./nplh-message-handler-ui/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
