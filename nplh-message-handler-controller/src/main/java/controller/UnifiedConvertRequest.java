a6efdde1847e9470653d25c0cd2e833c66d4f05b
sxherraj <javier.herrad@roche.com>
2025-10-29 10:58:12 +0100

    fixes

--- nplh-message-handler-controller/src/main/java/controller/UnifiedConvertRequest.java
+++ nplh-message-handler-controller/src/main/java/controller/UnifiedConvertRequest.java
@@ -2,25 +2,23 @@
 package controller;
 
 import lombok.Data;
-// TODO: Uncomment when domain classes are available
-// import org.example.domain.message.Message;
-// import org.example.domain.message.entity.Block;
-// import org.example.domain.message.entity.Slide;
-// import org.example.domain.message.entity.Specimen;
+ import org.example.domain.message.Message;
+ import org.example.domain.message.entity.Block;
+import org.example.domain.message.entity.Slide;
+import org.example.domain.message.entity.Specimen;
 
 /**
  * Unified request class that can handle all types of message conversions
  */
 @Data
 public class UnifiedConvertRequest {
-    // TODO: Uncomment when domain classes are available
-    // private Message message;
+    private Message message;
     private String messageType;
     private String hostName;
     private String hostType;
-    // private Specimen specimen;
-    // private Block block;
-    // private Slide slide;
+    private Specimen specimen;
+    private Block block;
+    private Slide slide;
     private String status;
 
 }
